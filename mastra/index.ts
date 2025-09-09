9e04b03da981c8564340af1dda1e08c12a6d577b
BASIL ALBADER <basilalbapro@gmail.com>
2025-09-09 15:51:23 +0200

    add mastra vercel deployer dependency

--- mastra/index.ts
+++ mastra/index.ts
@@ -1,19 +1,20 @@
-
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
 import { weatherWorkflow } from './workflows/weather-workflow';
 import { weatherAgent } from './agents/weather-agent';
+import { VercelDeployer } from '@mastra/deployer-vercel';
 
 export const mastra = new Mastra({
-  workflows: { weatherWorkflow },
-  agents: { weatherAgent },
-  storage: new LibSQLStore({
-    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
-  }),
-  logger: new PinoLogger({
-    name: 'Mastra',
-    level: 'info',
-  }),
+	workflows: { weatherWorkflow },
+	agents: { weatherAgent },
+	storage: new LibSQLStore({
+		// stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+		url: ':memory:',
+	}),
+	logger: new PinoLogger({
+		name: 'Mastra',
+		level: 'info',
+	}),
+	deployer: new VercelDeployer(),
 });
