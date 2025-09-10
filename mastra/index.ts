baeed9816a4221d0c46e5a2bacb9cb4e1054d769
BASIL ALBADER <basilalbapro@gmail.com>
2025-09-10 18:31:34 +0200

    add pdf to questions generator agent

--- mastra/index.ts
+++ mastra/index.ts
@@ -2,14 +2,16 @@ import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
 import { VercelDeployer } from '@mastra/deployer-vercel';
-import { frontendWorkflow } from './workflows/frontend-workflow';
-import { backendWorkflow } from './workflows/backend-workflow';
 import { frontendAgent } from './agents/frontend-agent';
 import { backendAgent } from './agents/backend-agent';
+import { pdfToQuestionsWorkflow } from './workflows/generate-questions-from-pdf-workflow';
+import { textQuestionAgent } from './agents/text-question-agent';
+import { pdfQuestionAgent } from './agents/pdf-question-agent';
+import { pdfSummarizationAgent } from './agents/pdf-summarization-agent';
 
 export const mastra = new Mastra({
-	workflows: { frontendWorkflow, backendWorkflow },
-	agents: { frontendAgent, backendAgent },
+	workflows: { pdfToQuestionsWorkflow },
+	agents: { textQuestionAgent, pdfQuestionAgent, pdfSummarizationAgent, frontendAgent, backendAgent },
 	storage: new LibSQLStore({
 		// stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
 		url: ':memory:',
