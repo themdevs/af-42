2c22f92ca2ad756a519fa45553100d2f49436830
BASIL ALBADER <basilalbapro@gmail.com>
2025-09-13 17:04:25 +0200

    WIP

---
+++ app/(users)/company/[company-name]/(challenges)/challenge/generate/page.tsx
@@ -0,0 +1,5 @@
+import { FormGenerator } from './FormGenerator';
+
+export default function GenerateChallengePage() {
+	return <FormGenerator />;
+}
--- components/layouts/company-layout.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import CompanyLayout from '@/components/layouts/company-layout';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-	return <CompanyLayout>{children}</CompanyLayout>;
-}
