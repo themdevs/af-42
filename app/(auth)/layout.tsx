75e028197d03c52265b1344f8744bb64e828ace2
BASIL ALBADER <basilalbapro@gmail.com>
2025-09-13 14:09:03 +0200

    add auth sign up - sign in pages / routes

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+
+'use client';
+
+import MainLayout from '@/components/layouts/main-layout';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return <MainLayout>{children}</MainLayout>;
+}
